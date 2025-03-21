71a3030f9afa14b0dfd49b5e1efa6b7c7a2ba964
abiodunnoah <ajaoolasco@gmail.com>
2025-03-21 13:38:32 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <!-- <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink> -->
+  </nav>
+
+  <RouterView />
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <main>HI</main>
+</template>
